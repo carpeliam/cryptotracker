d39841a763f94862a33ba7b805ad8243505d0bf6
Sean Olszewski <solszewski@pivotal.io>
2018-04-04 21:11:16 -0400

    WIP DO NOT MERGE OTHERWISE HEADLESS BALLS
    ok.
    - chartjs? we are both using CDN as well as installing the package, let's pick one.
    - we have a test that's failing - probably should be passing, though! looks like taking a screenshot and comparing it against a pre-fab image isn't quite perfect...
      - look at https://meowni.ca/posts/2017-puppeteer-tests/ which involves using pixelmatch, looks promising!

--- spec/JourneySpec.js
+++ spec/JourneySpec.js
@@ -1,13 +1,19 @@
 import puppeteer from 'puppeteer';
+import path from 'path';
+import fs from 'fs';
 
 describe('cryptotracker', () => {
   it('displays a graph', async () => {
+    const testImg = fs.readFileSync(path.join(__dirname, 'bargraph.png'));
+    const url = path.join(__dirname, '..', 'dist', 'index.html');
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('http://www.chartjs.org/samples/latest/charts/line/basic.html');
+    await page.goto(`file://${url}`);
 
-    const graph = await page.waitFor('canvas');
+    const graph = await page.waitFor('canvas.all-done');
     const screenshot = await graph.screenshot();
     expect(screenshot.filter(c => c == 0).length).toBeGreaterThan(0);
+    console.log(screenshot, testImg);
+    expect(screenshot.toString()).toEqual(testImg.toString());
   });
 });
